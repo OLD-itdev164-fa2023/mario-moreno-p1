4f6491993775542e2a5acf5ba59d988915f9a53f
morenm14 <morenm14@gmatc.matc.edu>
2022-09-29 10:51:15 -0500

    displayed products in homepage

--- eStore/src/pages/index.js
+++ eStore/src/pages/index.js
@@ -1,14 +1,45 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Product from "../components/product"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hello world</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulProduct {
+        edges {
+          node {
+            id
+            title
+            price
+            slug
+            rating {
+              rate
+              count
+            }
+            image {
+              gatsbyImageData(
+                layout: CONSTRAINED
+                placeholder: TRACED_SVG
+                width: 200
+              )
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      {data.allContentfulProduct.edges.map(edge => {
+        return <Product edge={edge} key={edge.node.id} />
+      })}
+    </Layout>
+  )
+}
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
