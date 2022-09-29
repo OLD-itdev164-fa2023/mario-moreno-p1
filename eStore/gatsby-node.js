05c22e33798d66d7786b39616fba74a2206b1c4c
morenm14 <morenm14@gmatc.matc.edu>
2022-09-29 12:29:06 -0500

    created pages in gatsby node

--- eStore/gatsby-node.js
+++ eStore/gatsby-node.js
@@ -1,5 +1,29 @@
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allContentfulProduct {
+        edges {
+          node {
+            title
+            slug
+            id
+          }
+        }
+      }
+    }
+  `)
+  result.data.allContentfulProduct.edges.forEach(edge => {
+    createPage({
+      path: `/product/${edge.node.slug}`,
+      component: require.resolve("./src/templates/SingleProduct.js"),
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
